8d6e4f1dcff40d48a248e3f1c96835704817de0f
Michael <mitru0217@gmail.com>
2022-10-10 22:26:41 +0300

    в процессе

--- src/api-service.js
+++ src/api-service.js
@@ -1,31 +1,44 @@
-export default class ImageApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
-  fetchImages() {
-    const url = `https://pixabay.com/api/?key=30307966-ea2e6055e88053146b4d64f93&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=20`;
-    return fetch(url)
-      .then(r => {
-        if (!r.ok) {
-          throw new Error(mistake);
-        }
-        return r.json();
-      })
-      .then(data => {
-        this.page += 1;
-        return data.hits;
-      });
-  }
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-  resetPage() {
-    this.page = 1;
-  }
+// const BASE_URL = 'https://pixabay.com/api';
+// const API_KEY = '30307966-ea2e6055e88053146b4d64f93';
+// export default class ImageApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+//   fetchImages() {
+//     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=20`;
+//     return fetch(url)
+//       .then(r => {
+//         if (!r.ok) {
+//           throw new Error(mistake);
+//         }
+//         return r.json();
+//       })
+//       .then(({ total, totalHits, hits }) => {
+//         this.page += 1;
+//         if (totalHits === 0) {
+//           Notify.failure(
+//             `❌Sorry, there are no images matching your search query. Please try again.`
+//           );
+//         } else {
+//           Notify.info(`Hooray! We found ${totalHits} images.`);
+//         }
 
-  get query() {
-    return this.searchQuery;
-  }
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-}
+//         return hits;
+//       });
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-// const axios = require('axios');
-// const BASE_URL = 'https://pixabay.com/api/';
-import ImageApiService from './api-service.js';
+// import ImageApiService from './api-service.js';
+import ImageApiService from './axiosApiService.js';
 import photoCards from './templates/photos.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('.js-search-form');
 let photosContainer = document.querySelector('.js-photos-container');
@@ -20,119 +21,52 @@ function onSearch(e) {
   imageApiService.query = e.currentTarget.elements.query.value.trim();
 
   if (imageApiService.query === '') {
+    clearPhotosContainer();
     return alert('Fill the field');
   }
 
   imageApiService.resetPage();
 
-  imageApiService
-    .fetchImages()
-    .then(hits => {
-      clearPhotosContainer();
-      appendPhotosMarkup(hits);
-    })
-    .catch(error => {
-      if (Array.length === 0) {
-        Notify.info(
-          `Sorry, there are no images matching your search query. Please try again.`
-        );
-      }
-    });
+  imageApiService.fetchImages().then(({ totalHits, hits }) => {
+    if (hits.length === 0) {
+      Notify.failure(
+        `❌Sorry, there are no images matching your search query. Please try again.`
+      );
+    }
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+    console.log(hits);
+    clearPhotosContainer();
+    appendPhotosMarkup(hits);
+  });
 }
 
 function onLoadMore() {
+  lightbox.refresh();
   imageApiService.fetchImages().then(appendPhotosMarkup);
 }
 
 function appendPhotosMarkup(hits) {
   photosContainer.insertAdjacentHTML('beforeend', photoCards(hits));
+
+  let lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
 }
+
 function clearPhotosContainer() {
   photosContainer.innerHTML = '';
 }
 
-function markup({
-  hits: [
-    { webformatURL, largeImageURL, tags, likes, views, comments, downloads },
-  ],
-}) {
-  photosContainer.innerHTML = `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img-item"/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:</b>
-      <span>${likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views:</b>
-      <span>${views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments:</b>
-      <span>${comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads:</b>
-      <span>${downloads}</span>
-    </p>
-  </div>
-</div>
-  `;
-}
-
-// key - твой уникальный ключ доступа к API.
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-// safesearch - фильтр по возрасту. Задай значение true.
-
-// В ответе будет массив изображений удовлетворивших критериям параметров запроса. Каждое изображение описывается объектом, из которого тебе интересны только следующие свойства:
-
-// webformatURL - ссылка на маленькое изображение для списка карточек.
-// largeImageURL - ссылка на большое изображение.
-// tags - строка с описанием изображения. Подойдет для атрибута alt.
-// likes - количество лайков.
-// views - количество просмотров.
-// comments - количество комментариев.
-// downloads - количество загрузок.
-// Если бэкенд возвращает пустой массив, значит ничего подходящего найдено небыло.
-//  В таком случае показывай уведомление с текстом "Sorry, there are no images matching your search query. Please try again.". Для уведомлений используй библиотеку notiflix.
-// import './css/styles.css';
-// import cardsList from './templates/cards-list.hbs';
-// import cards from './templates/cards.hbs';
-// import debounce from 'lodash.debounce';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import 'notiflix/dist/notiflix-3.2.5.min.css';
-// import fetchCountries from './fetchCountries';
-
-// const DEBOUNCE_DELAY = 300;
-// const query = document.querySelector('#search-box');
-// let countryList = document.querySelector('.country-list');
-
-// query.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
-// function onInputChange(e) {
-//   e.preventDefault();
-//   const searchQuery = e.target.value.trim();
-
-//   if (searchQuery != '') {
-//     fetchCountries(searchQuery)
-//       .then(country => {
-//         if (country.length > 10) {
-//           Notify.info(
-//             ` Too many matches found. Please enter a more specific name.`
-//           );
-//           countryList.innerHTML = '';
-//         } else if (country.length >= 2) {
-//           countryList.innerHTML = cardsList(country);
-//         } else {
-//           countryList.innerHTML = cards(country);
-//         }
-//       })
-//       .catch(error => {
-//         console.log(error);
-//         Notify.failure(`❌ Oops, there is no country with that name`);
-//         countryList.innerHTML = '';
-//       });
+// .then(response => {
+//   return response;
+// })
+// .then(({ data: { total, totalHits, hits } }) => {
+//   this.page += 1;
+//   if (totalHits === 0) {
+//     Notify.failure(
+//       `❌Sorry, there are no images matching your search query. Please try again.`
+//     );
+//   } else {
+//     Notify.info(`Hooray! We found ${totalHits} images.`);
 //   }
-//   return (countryList.innerHTML = '');
-// }
+
+//   return hits;
