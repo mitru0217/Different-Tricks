e3e2a0a6bc8baeda15d195a6dde1d37ce1d90b7c
Michael <mitru0217@gmail.com>
2022-10-11 03:31:22 +0300

    делаю бесконечный скролл

--- src/index.js
+++ src/index.js
@@ -8,13 +8,38 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('.js-search-form');
 let photosContainer = document.querySelector('.js-photos-container');
-const loadMoreBtn = document.querySelector('.btn');
-
-searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', onLoadMore);
+const searchBtn = document.querySelector('.search-button');
+const toBtnTop = document.querySelector('.btn-to-top');
+const loading = document.querySelector('.pulse-container');
+const guard = document.querySelector('.js-guard');
+// window.addEventListener('scroll', onScroll);
+toBtnTop.addEventListener('click', onToTopBtn);
 
 const imageApiService = new ImageApiService();
 
+const observeOptions = {
+  root: null,
+  rootMargin: '300px',
+  threshhold: 1,
+};
+
+function updateGallery(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      imageApiService.fetchImages().then(hits => {
+        // lightbox.refresh();
+        photoCards(hits);
+        appendPhotosMarkup(hits);
+      });
+    }
+  });
+}
+const observer = new IntersectionObserver(updateGallery, observeOptions);
+
+// let simpleLightBox = null;
+// let totalPics = 0;
+
+searchForm.addEventListener('submit', onSearch);
 function onSearch(e) {
   e.preventDefault();
 
@@ -24,49 +49,87 @@ function onSearch(e) {
     clearPhotosContainer();
     return alert('Fill the field');
   }
+  // searchBtn.disabled = false;
+  // observer.unobserve(loading);
+
+  clearPhotosContainer();
 
   imageApiService.resetPage();
 
   imageApiService.fetchImages().then(({ totalHits, hits }) => {
     if (hits.length === 0) {
+      clearPhotosContainer();
       Notify.failure(
         `❌Sorry, there are no images matching your search query. Please try again.`
       );
+    } else {
+      appendPhotosMarkup(hits);
+      observer.observe(guard);
+      Notify.info(`Hooray! We found ${totalHits} images.`);
     }
-    Notify.info(`Hooray! We found ${totalHits} images.`);
+
+    // const totalPages = Math.ceil(totalHits / imageApiService.perPage);
+    // if (imageApiService.page > totalPages) {
+    //   Notify.warning(
+    //     "We're sorry, but you've reached the end of search results."
+    //   );
+    // }
+    // smoothScroll();
     console.log(hits);
-    clearPhotosContainer();
-    appendPhotosMarkup(hits);
   });
 }
 
-function onLoadMore() {
-  lightbox.refresh();
-  imageApiService.fetchImages().then(appendPhotosMarkup);
-}
-
 function appendPhotosMarkup(hits) {
+  // lightbox.refresh();
   photosContainer.insertAdjacentHTML('beforeend', photoCards(hits));
 
   let lightbox = new SimpleLightbox('.gallery a');
-  lightbox.refresh();
+  // lightbox.refresh('show.SimpleLightbox');
+  // loading.classList.remove('show');
+  // observer.observe(loading);
 }
 
 function clearPhotosContainer() {
   photosContainer.innerHTML = '';
 }
 
-// .then(response => {
-//   return response;
-// })
-// .then(({ data: { total, totalHits, hits } }) => {
-//   this.page += 1;
-//   if (totalHits === 0) {
-//     Notify.failure(
-//       `❌Sorry, there are no images matching your search query. Please try again.`
-//     );
-//   } else {
-//     Notify.info(`Hooray! We found ${totalHits} images.`);
-//   }
+// function showLoading() {
+//   loading.classList.add('show');
 
-//   return hits;
+//   setTimeout(loadMore, 1500);
+// }
+
+// function loadMore() {
+//   if (imageApiService.page > Math.ceil(totalPics / imageApiService.perPage)) {
+//     loading.classList.remove('show');
+//     return;
+//   }
+// }
+// function onScroll() {
+//   const scrolled = window.pageYOffset;
+//   const coords = document.documentElement.clientHeight;
+
+//   if (scrolled > coords) {
+//     toBtnTop.classList.add('btn-to-top--visible');
+//   }
+//   if (scrolled < coords) {
+//     toBtnTop.classList.remove('btn-to-top--visible');
+//   }
+// }
+
+function onToTopBtn() {
+  if (window.pageYOffset > 0) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
+
+// function smoothScroll() {
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
